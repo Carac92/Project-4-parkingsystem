4baaeb8640654df2f8727074c56fec075202856e
Quentin Caracatzanis <quentinversaille@gmail.com>
2022-10-18 11:52:30 +0200

    Test added
    added verification at processExitingVehicleTest, I used ArgumentCaptor that I forgot to use before.
    
    added a test processIncomingVehicleTest, I also used ArgumentCaptor.
    
    Goals of coverage 69% on service, 61% on model

--- src/test/java/com/parkit/parkingsystem/ParkingServiceTest.java
+++ src/test/java/com/parkit/parkingsystem/ParkingServiceTest.java
@@ -10,10 +10,15 @@ import com.parkit.parkingsystem.util.InputReaderUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
 import java.time.LocalDateTime;
+
+import static com.parkit.parkingsystem.constants.ParkingType.CAR;
+import static junit.framework.Assert.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -32,17 +37,6 @@ public class ParkingServiceTest {
     private void setUpPerTest() {
         try {
             when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
-
-            ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR,false);
-            Ticket ticket = new Ticket();
-            ticket.setInTime(LocalDateTime.now().minusHours(1));
-            ticket.setParkingSpot(parkingSpot);
-            ticket.setVehicleRegNumber("ABCDEF");
-            when(ticketDAO.getTicket(anyString())).thenReturn(ticket);
-            when(ticketDAO.updateTicket(any(Ticket.class))).thenReturn(true);
-
-            when(parkingSpotDAO.updateParking(any(ParkingSpot.class))).thenReturn(true);
-
             parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         } catch (Exception e) {
             e.printStackTrace();
@@ -51,13 +45,57 @@ public class ParkingServiceTest {
     }
 
     @Test
+    public void processIncomingVehicleTest(){
+        //Given
+        when(inputReaderUtil.readSelection()).thenReturn(1);
+        when(parkingSpotDAO.getNextAvailableSlot(CAR)).thenReturn(1);
+
+        // WHEN
+        parkingService.processIncomingVehicle();
+
+        // THEN
+        ArgumentCaptor<ParkingSpot> parkingSpotArgumentCaptor = ArgumentCaptor.forClass(ParkingSpot.class);
+        ArgumentCaptor<Ticket> ticketArgumentCaptor = ArgumentCaptor.forClass(Ticket.class);
+        verify(parkingSpotDAO, Mockito.times(1)).updateParking((parkingSpotArgumentCaptor.capture()));
+        verify(ticketDAO, Mockito.times(1)).saveTicket((ticketArgumentCaptor.capture()));
+        ParkingSpot updatedParkingSpot= parkingSpotArgumentCaptor.getValue();
+        Ticket updatedTicket= ticketArgumentCaptor.getValue();
+        assertEquals(updatedParkingSpot.isAvailable(), false);
+        assertEquals(updatedParkingSpot.getParkingType(),CAR);
+        assertEquals(updatedParkingSpot.getId(), 1);
+        assertNotNull(updatedTicket.getInTime());
+        assertEquals(updatedTicket.getOutTime(), null);
+        assertEquals(1,updatedTicket.getParkingSpot().getId());
+        assertEquals(updatedTicket.getPrice(), 0.0);
+        assertEquals(updatedTicket.getVehicleRegNumber(),"ABCDEF");
+        assertEquals(updatedTicket.getId(),0);
+        assertEquals(updatedTicket.getParkingSpot().getId(),updatedParkingSpot.getId());
+    }
+    @Test
     public void processExitingVehicleTest(){
+        // GIVEN
+        ParkingSpot parkingSpot = new ParkingSpot(1, CAR,false);
+        Ticket ticket = new Ticket();
+        ticket.setInTime(LocalDateTime.now().minusHours(1));
+        ticket.setParkingSpot(parkingSpot);
+        ticket.setVehicleRegNumber("ABCDEF");
+        when(ticketDAO.getTicket(anyString())).thenReturn(ticket);
+        when(ticketDAO.updateTicket(any(Ticket.class))).thenReturn(true);
+        when(parkingSpotDAO.updateParking(any(ParkingSpot.class))).thenReturn(true);
 
         // WHEN
         parkingService.processExitingVehicle();
         // THEN
-        verify(parkingSpotDAO, Mockito.times(1)).updateParking(any(ParkingSpot.class));
-        verify(ticketDAO, Mockito.times(1)).updateTicket(any(Ticket.class));
+        ArgumentCaptor<ParkingSpot> parkingSpotArgumentCaptor = ArgumentCaptor.forClass(ParkingSpot.class);
+        ArgumentCaptor<Ticket> ticketArgumentCaptor = ArgumentCaptor.forClass(Ticket.class);
+        verify(parkingSpotDAO, Mockito.times(1)).updateParking(parkingSpotArgumentCaptor.capture());
+        verify(ticketDAO, Mockito.times(1)).updateTicket((ticketArgumentCaptor.capture()));
+        ParkingSpot updatedParkingSpot = parkingSpotArgumentCaptor.getValue();
+        Ticket updatedTicket = ticketArgumentCaptor.getValue();
+        assertEquals(updatedParkingSpot.isAvailable(), true);
+        assertNotNull(updatedTicket.getOutTime());
+        assertEquals(updatedTicket.getPrice(),1.5, 0.001);
+        assertEquals(ticketDAO.getTicket("ABCDEF").getPrice(),1.5, 0.001);
+        assertNotNull(ticketDAO.getTicket("ABCDEF").getOutTime());
     }
-
 }
